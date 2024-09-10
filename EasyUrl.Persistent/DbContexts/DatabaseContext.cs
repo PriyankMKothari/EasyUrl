5ef26bf08002ea833dec3a34c0da7f0cc9cb9114
GHDNET\pkothari <priyank.kothari@ghd.com>
2024-09-10 22:06:32 +1000

    Corrected missing file references.

---
+++ EasyUrl.Persistent/DbContexts/DatabaseContext.cs
@@ -0,0 +1,39 @@
+﻿using EasyUrl.Persistent.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyUrl.Persistent.DbContexts;
+
+public sealed class DatabaseContext : DbContext
+{
+    public DbSet<Tag> Tags { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+        base.Database.EnsureCreated();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<Tag>().ToTable("Tag");
+        builder.Entity<Tag>().HasKey(tag => tag.Id);
+        builder.Entity<Tag>().Property(tag => tag.Id).ValueGeneratedOnAdd();
+
+        builder.Entity<Tag>().Property(tag => tag.Code).IsRequired();
+        builder.Entity<Tag>().Property(tag => tag.Code).HasMaxLength(10);
+
+        builder.Entity<Tag>().Property(tag => tag.Url).IsRequired();
+        builder.Entity<Tag>().Property(tag => tag.Url).HasMaxLength(250);
+    }
+
+    public override Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken)
+    {
+        // set CreatedDate when entity is EntityState.Added.
+        var addedEntities = ChangeTracker.Entries().Where(entities => entities.State == EntityState.Added).ToList();
+        addedEntities.ForEach(entity =>
+        {
+            entity.Property("CreatedDate").CurrentValue = DateTimeOffset.Now.ToLocalTime();
+        });
+
+        return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+    }
+}
---
+++ EasyUrl.Persistent/Repositories/ITagRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using EasyUrl.Persistent.Entities;
+
+namespace EasyUrl.Persistent.Repositories;
+
+/// <summary>
+/// Repository to perform storage implementation for <see cref="Tag" />.
+/// </summary>
+public interface ITagRepository
+{
+    /// <summary>
+    /// Adds <see cref="Tag" /> to the database.
+    /// </summary>
+    /// <param name="tag"><see cref="Tag" /> to add.</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns>Newly created <see cref="Tag" />.</returns>
+    Task<Tag> CreateAsync(Tag tag, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Deletes existing <see cref="Tag" />.
+    /// </summary>
+    /// <param name="tag"><see cref="Tag" /> to delete</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns><see cref="Task" />.</returns>
+    Task<Tag> DeleteAsync(Tag tag, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Checks if any <see cref="Tag" /> exists that match(es) condition(s).
+    /// </summary>
+    /// <param name="predicateExpression">Condition(s) to match</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns><see cref="Task{bool}" />.</returns>
+    Task<bool> ExistsAsync(Expression<Func<Tag, bool>> predicateExpression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Gets a tag that match(es) the condition(s).
+    /// </summary>
+    /// <param name="predicateExpression">Condition(s) to match</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns><see cref="Task{Tag}" />.</returns>
+    Task<Tag?> GetAsync(Expression<Func<Tag, bool>> predicateExpression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Gets all <see cref="Tag" />s that match(es) the condition(s).
+    /// </summary>
+    /// <param name="predicateExpression">Condition(s) to match.</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns><see cref="Task{IList<see cref="Tag" />}" />.</returns>
+    Task<IList<Tag>> GetAllAsync(Expression<Func<Tag, bool>> predicateExpression, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Updates an existing <see cref="Tag" />.
+    /// </summary>
+    /// <param name="tag"><see cref="Tag" /> to update</param>
+    /// <param name="cancellationToken"><see cref="CancellationToken" />.</param>
+    /// <returns>Updated <see cref="Tag" />.</returns>
+    Task<Tag> UpdateAsync(Tag tag, CancellationToken cancellationToken);
+}
